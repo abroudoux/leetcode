b35e80056aa9e65cf67209e275508186299decf9
arthur <arthur.broudoux@gmail.com>
2024-02-06 21:33:16 +0100

    #1

--- easy/first-unique-car.js
+++ easy/first-unique-car.js
@@ -1,4 +1,8 @@
 /**
+ * #387 First Unique Character in a String
+ *
+ * Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+ *
  * @param {string} s
  * @return {number}
  */
---
+++ easy/two-sum.js
@@ -0,0 +1,33 @@
+/**
+ * #1 Two Sum
+ *
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    console.log("tour : " + i);
+    console.log(nums[i]);
+    for (let j = i + 1; j < nums.length; j++) {
+      console.log(nums[j]);
+      let totalSum = nums[j] + nums[i];
+      if (totalSum === target) {
+        let indicesResponses = [i, j];
+        console.log("Target : " + indicesResponses);
+      }
+    }
+    console.log("=========");
+  }
+};
+
+console.log("TEST 1");
+twoSum([2, 7, 11, 15], 9);
+console.log("TEST 2");
+twoSum([3, 2, 4], 6);
+console.log("TEST 3");
+twoSum([[3, 3], 6]);
